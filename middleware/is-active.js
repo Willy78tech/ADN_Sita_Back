e79fefeb377378e942de1646c2df9877f55fd561
Willy Dian <willy78.dian@gmail.com>
2022-12-18 23:16:29 -0500

    Changement lien activation de compte

--- middleware/is-active.js
+++ middleware/is-active.js
@@ -1,8 +1,6 @@
 "Use strict";
 
-// const jwt = require('jsonwebtoken');
-// const dotenv = require('dotenv');
-// dotenv.config();
+
 const User = require("../models/user");
 
 module.exports = (req, res, next) => {
@@ -25,26 +23,3 @@ module.exports = (req, res, next) => {
     });
 };
 
-// middleware de confirmation de l'inscription
-// module.exports = (req, res, next) => {   
-//     const token = req.params.token;
-//     User.findOne({ token: token
-//      })
-//      .then((user) => {
-//        if (!user) {
-//          const error = new Error("No user found");
-//          error.statusCode = 404;
-//          throw error;
-//        }
-//        return user.updateOne({ token: null, isActive: true });
-//      })
-//      .then((user) => {
-//        res.status(200).json({ message: "User confirmed", user: user });
-//      })
-//      .catch((err) => {
-//        if (!err.statusCode) {
-//          err.statusCode = 500;
-//        }
-//        next(err);
-//      });
-//  };
